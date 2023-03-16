57ccf638ae9161ee3d32eb5a6ed868afd90f8751
amejid <abdelmejid89@gmail.com>
2023-03-16 12:55:33 +0300

    Update models to remove authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  validates :ip_address, presence: true, uniqueness: true
 
   has_many :searches
 end
